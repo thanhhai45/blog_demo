9978de88f69d9dcf332a28dfbf6c04063602304e
thanhhai45 <thanhhai@itviec.com>
2024-08-20 23:05:05 +0700

    add action text in rails

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
 
   belongs_to :user
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true, length: { minimum: 5, maximum: 50 }
   validates :body, presence: true, length: { minimum: 10, maximum: 1000 }
